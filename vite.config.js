5928a40b438b642ff3b59a0786152861ba308aff
Rahul Vijayakumar <rahulvijayakumarpt4551@gmail.com>
2025-02-22 11:35:53 +0530

    update homepage URL and configure base path for custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/", // set base for deployment to custom domain root
   plugins: [react()],
-  base: '/ca-landing-page/', // Add this line - should match your repository name
 })
