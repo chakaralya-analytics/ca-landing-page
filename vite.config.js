ebcbfcacfc2657f60e81618ed8fb017ec694eeed
Rahul Vijayakumar <rahulvijayakumarpt4551@gmail.com>
2025-02-22 11:03:37 +0530

    add github actions worflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ca-landing-page/', // Add this line
+  base: '/ca-landing-page/', // Add this line - should match your repository name
 })
